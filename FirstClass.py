85d2f576c7eff61a4b54ed97a9299d34d38bde9a
Gurgen Avagyan <gugo.avagyan@gmail.com>
2018-07-30 18:02:43 +0400

    fitst commit

---
+++ FirstClass.py
@@ -0,0 +1,33 @@
+class FirstClass:
+	def __init__(self,name):
+		self.name = name
+	def setdata(self,value):
+		self.data = value
+		
+	def display(self):
+		print(" vaule of {} =".format(self.name), self.data)
+
+
+x = FirstClass('first')
+y = FirstClass('second')
+
+x.setdata("GUrgen")
+y.setdata(3.14)
+
+x.display()
+y.display()
+
+# x.anotherspam = 'sam'
+# print(x.anotherspam)
+
+# print (y.anotherspam)
+
+class SecondClass(FirstClass):
+	def __init__(self):
+		pass
+	def display(self):
+		print('Currnet value = {}'.format(self.data))
+
+b = SecondClass()
+b.setdata(313)
+b.display()
